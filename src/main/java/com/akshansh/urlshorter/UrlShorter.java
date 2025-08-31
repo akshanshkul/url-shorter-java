4116c6e08d90dd77f53ce8ec4f60d4d480260c42
Akshansh <akshanshkul7830@gmail.com>
2025-08-31 20:13:33 +0530

    first

---
+++ src/main/java/com/akshansh/urlshorter/UrlShorter.java
@@ -0,0 +1,13 @@
+package com.akshansh.urlshorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorter.class, args);
+	}
+
+}
